89bebc02e36d8100ab1381e3d458c37098cfe06b
Hbyrd4 <123094848+Hbyrd4@users.noreply.github.com>
2023-04-20 09:47:05 -0500

    scroll magic stuff works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-// JS scripts placed here
+(function () {
+
+    var controller = new ScrollMagic.Controller({
+              globalSceneOptions: {
+                  triggerHook: 'onLeave',
+                  duration: "100%"
+              }
+      });
+       var slides = document.querySelectorAll("section.panel");
+  
+      for (var i=0; i<slides.length; i++) {
+          var scene = new ScrollMagic.Scene({
+              triggerElement: slides[i]
+          })
+          .setPin(slides[i], {pushFollowers: false})
+          .addIndicators() 
+          .addTo(controller);
+      }
+      
+  
+  }())
