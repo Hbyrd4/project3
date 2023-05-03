5b0322a7a6c729d65fdb297b7f34d77457dc1923
Hbyrd4 <123094848+Hbyrd4@users.noreply.github.com>
2023-05-03 17:34:08 -0500

    added js for moblie

--- js/main.js
+++ js/main.js
@@ -19,3 +19,12 @@
       
   
   }())
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
